b487d8d40f5c38377d2ad4070523f4482548f2b9
MaxDev <86471587+MaximeG44270@users.noreply.github.com>
2024-10-25 16:07:06 +0200

    Commit 12
    Réglage footer et contact-form

--- src/Component/Contact-form/Contact-form.tsx
+++ src/Component/Contact-form/Contact-form.tsx
@@ -13,7 +13,7 @@ const ContactForm: React.FC = () => {
           style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }}
         ></iframe>
       </div>
-      <div className="container px-5 py-24 mx-auto flex">
+      <div className="container padding-top px-5 py-24 mx-auto flex">
         <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md z-index">
           <h2 className="text-gray-900 text-lg mb-1 font-medium title-font text-h2">Cheery Dolls</h2>
           <div className="relative mb-4">
